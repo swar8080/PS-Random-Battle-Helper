c1b80242eee9daaba3d13360cf43601ce82ec975
Steven Swartz <stevenswartz@live.ca>
2020-06-06 19:18:14 -0400

    disable analytics during local development

--- client/src/app/useAnalytics.ts
+++ client/src/app/useAnalytics.ts
@@ -3,35 +3,41 @@
  */
 import * as React from "react";
 
+const IS_ENABLED = !!process.env.REACT_APP_ENABLE_GA;
+
 const useAnalytics = () => {
     const isFirstSearch = React.useRef(true);
 
     const trackSearchChange = (search: Common.PokemonSummarySearchInputs) => {
-        if (!isFirstSearch.current){
-            window.ga("send", {
-                hitType: "event",
-                eventCategory: "search-gen",
-                eventAction: "change",
-                eventLabel: search.generation
-            });
+        if (IS_ENABLED){
+            if (!isFirstSearch.current){
+                window.ga("send", {
+                    hitType: "event",
+                    eventCategory: "search-gen",
+                    eventAction: "change",
+                    eventLabel: search.generation
+                });
+            }
+            isFirstSearch.current = false;
         }
-        isFirstSearch.current = false;
     }
 
     return {trackSearchChange}
 }
 
 const trackException = (err: Error, fatal: boolean = false, stack?: string) => {
-    let description = err.message;
-    const stackTrace = stack || err.stack;
-    if (stackTrace){
-        description += " " + stackTrace;
+    if (IS_ENABLED){
+        let description = err.message;
+        const stackTrace = stack || err.stack;
+        if (stackTrace){
+            description += " " + stackTrace;
+        }
+        
+        window.ga("send", "exception", {
+            exDescription: description,
+            exFatal: fatal
+        })
     }
-    
-    window.ga("send", "exception", {
-        exDescription: description,
-        exFatal: fatal
-    })
 }
 
 export {useAnalytics, trackException}
